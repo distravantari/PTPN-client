fefbe5d601e49429f55b237af746ee045d5d0adb
krisnadi <Krisnadi@node8.net>
2016-03-18 16:15:08 +0700

    v9.1

--- assets/admin/pages/scripts/table-editable.js
+++ assets/admin/pages/scripts/table-editable.js
@@ -71,6 +71,11 @@ var TableEditable = function () {
             // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js).
             // So when dropdowns used the scrollable div should be removed.
             //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+            "dom": 'T<"clear">lrtip',
+
+            "tableTools": {           
+               "sSwfPath": "assets/global/plugins/datatables/extensions/TableTools/swf/copy_csv_xls_pdf.swf"
+            },
 
             "lengthMenu": [
                 [5, 15, 20, -1],
